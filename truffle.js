e5a34f25a636a3c84fc2ff4ceaefb67a66d044a1
Albert Puigsech Galicia <albert@puigsech.com>
2017-07-26 01:00:01 +0200

    enable more ethereum networks for deployment

--- truffle.js
+++ truffle.js
@@ -1,10 +1,31 @@
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    "testrpc": {
+    	host: "localhost",
+    	port: 8545,
+    	network_id: "*" // Match any network id
+    },
+    "ethereum": {
+    	host: "localhost",
+    	port: 18545,
+    	network_id: 1,
+    },
+    "morden": {
+    	host: "localhost",
+    	port: 28545,
+    	network_id: 2,
+    },
+    "ropsten": {
+    	host: "localhost",
+    	port: 38545,
+    	network_id: 3,
+    },    
+    "rinkeby": {
+    	host: "localhost",
+    	port: 48545,
+    	network_id: 4,
+    	from: '0x0ec696187cb52E69C54E212B480E0998ef41056A',
     }
   }
 };
